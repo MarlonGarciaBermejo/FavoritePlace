b7fc987413c2a4540771262747ca4db70c3d1dff
Marlon <marlongarciab@hotmail.com>
2022-12-13 01:09:50 +0100

    "Added login screen with a layout design for xml file & a authentication login/create user for firebase, + a google maps activity"

--- app/src/main/java/com/firstapp/favoriteplace/DestinationActivity.kt
+++ app/src/main/java/com/firstapp/favoriteplace/DestinationActivity.kt
@@ -22,27 +22,19 @@ class DestinationActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityDestinationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val stockholm = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(stockholm).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm))
     }
 }
--- app/src/main/java/com/firstapp/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/firstapp/favoriteplace/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.firstapp.favoriteplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+    lateinit var emailView: EditText
+    lateinit var passwordView: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        auth = Firebase.auth
+        emailView = findViewById(R.id.emailTextView)
+        passwordView = findViewById(R.id.passwordTextView)
+
+
+
+        val createButton = findViewById<ImageButton>(R.id.createButton)
+        createButton.setOnClickListener {
+            createUser()
+        }
+        val loginButton = findViewById<ImageButton>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            loginUser()
+        }
     }
+    fun goToAddActivity(){
+        val intent = Intent(this, DestinationActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    fun loginUser() {
+        val email = emailView.text.toString()
+        val password = passwordView.text.toString()
+
+        if (email.isEmpty() || password.isEmpty())
+            return
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("!!!", "Sign in succeeded")
+                    goToAddActivity()
+                } else {
+                    Log.d("!!!", "user not signed in ${task.exception}")
+                }
+            }
+    }
+
+
+    fun createUser() {
+        val email = emailView.text.toString()
+        val password = passwordView.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+                Log.d("!!!", "Created user succeeded")
+                goToAddActivity()
+            } else {
+            Log.d("!!!", "user not created ${task.exception}")
+            }
+         }
+
+    }
+
 }
+
